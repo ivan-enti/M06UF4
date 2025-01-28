07724372cde323e3405b8165b25b237e7d967f0a
Ivan Gonzalez <ivan.gonzalez@enti.cat>
2025-01-28 19:56:22 +0100

    Falta terminar timer

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ let http_server = http.createServer(function (request, response) {
 let ws_server = new ws.Server({server: http_server});
 
 let player1, player2;
+
+let start_time = 5000;
+let second_unit = 1000;
+let max_score = 3;
+
 ws_server.on('connection', function (conn) {
 	console.log("Usuario conectado");	
 
@@ -31,6 +36,15 @@ ws_server.on('connection', function (conn) {
 		};
 
 		player1.send(JSON.stringify(info));
+
+		player1.on('close', function(){
+			console.log("Player 1 disconnected");
+			player1 = null;
+			if (player2 != null){
+				player2.send()
+			}
+		});
+
 		player1.on('message',function (msg) {
 			if (player2 == null)
 				return;
@@ -46,6 +60,19 @@ ws_server.on('connection', function (conn) {
 			}
 			else if(info.score1 != null){
 				player2.send(JSON.stringify(info))
+				if (info.score1 >= max_score || info.score2 >= max_score){
+					let data = {
+						game_over: true,
+						winner: 0
+					};
+					if (info.score1 >= 3)
+						data.winner = 1;
+					else
+						data.winner = 2;
+					player1.send(JSON.stringify(data));
+					player2.send(JSON.stringify(data));
+					return;
+				}
 			}
 		});
 	}
@@ -55,8 +82,36 @@ ws_server.on('connection', function (conn) {
 		let info = {
 			player_num: 2
 		};
-
 		player2.send(JSON.stringify(info));
+		
+		/*setTimeout(function(){
+			let game_info = {
+				game_start: true
+			};
+			player1.send(JSON.stringify(game_info));
+			player2.send(JSON.stringify(game_info));
+		}, start_time);*/
+
+		let count_timer = start_time / second_unit;
+		if(count_timer > 0){
+			setInterval(function(){
+				info = {
+					game_timer: count_timer,
+					game_start: count_timer == 0
+				}
+				player2.send(JSON.stringify(info));
+				if(player1 != null)
+					player1.send(JSON.stringify(info));
+
+				count_timer--;
+			}, second_unit);
+		}
+
+		player2.on('close', function(){
+			console.log("Player 2 disconnected");
+			player2 = null;
+		});
+
 		player2.on('message',function (msg) {
 			if (player1 == null)
 				return;
